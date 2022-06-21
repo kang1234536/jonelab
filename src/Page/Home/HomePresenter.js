ec7bbdf497525a93d593e2ae0bb137eeea63e1b5
hyunh84 <hyunh84@hanmail.net>
2022-06-21 14:08:26 +0900

    2022/06/21 작업

--- src/Page/Home/HomePresenter.js
+++ src/Page/Home/HomePresenter.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import IntroSection from "./IntroSection";
 
 const HomePresenter = ()=>{
+	const [observeEl, setObserveEl] = useState([]);
+	const options = {
+		rootMargin : '10px',
+		threshold:1
+	}
+	const callback = function(entries, observer) {
+		entries.forEach(function(entry) {
+			if (entry.isIntersecting) {
+				// console.log('화면에서 노출됨');
+				entry.target.classList.add('active');
+				observer.unobserve(entry.target);
+			} else {
+				// console.log('화면에서 제외됨');
+				//entry.target.classList.remove('active');
+			}
+		});
+	}
+	// const observer = new IntersectionObserver(callback, options);
+	const observer = useMemo(() => new IntersectionObserver(callback,options), []);
+
+	useEffect(()=>{
+		if(observeEl.length) {
+			observeEl.forEach(function(box) {
+				observer.observe(box);
+			});
+		}
+
+		return ()=>{
+			if(observeEl.length) observer.unobserve();
+		}
+	}, [observeEl]);
+
 	return(
 		<div className="contents">
 			{/* S : intro */}
-			<IntroSection />
+			<IntroSection setObserveEl = {setObserveEl} />
 			{/* E : intro */}
 			{/* S : intro */}
-			<IntroSection />
+			<IntroSection setObserveEl = {setObserveEl} />
 			{/* E : intro */}
 			{/* S : intro */}
-			<IntroSection />
+			<IntroSection setObserveEl = {setObserveEl} />
 			{/* E : intro */}
 			{/* S : intro */}
-			<IntroSection />
+			<IntroSection setObserveEl = {setObserveEl} />
 			{/* E : intro */}
 			{/* S : intro */}
-			<IntroSection />
+			<IntroSection setObserveEl = {setObserveEl} />
 			{/* E : intro */}
 		</div>
 	);
--- src/Page/Home/IntroSection/index.js
+++ src/Page/Home/IntroSection/index.js
@@ -1,20 +1,49 @@
-import React, { useRef, useEffect } from 'react'; 
-// import { useInView } from 'react-intersection-observer';
-// import PropTypes from 'prop-types';
+import React, { useRef, useEffect, useState, useMemo } from 'react'; 
+import PropTypes from 'prop-types';
 
-const IntroSection = ()=>{
-	const winH = window.innerHeight;
+const IntroSection = ({setObserveEl})=>{
+	const [loopIdx, setLoopIdx] = useState(null);
 	const introWrap = useRef(null);
+	let winH = window.innerHeight;
+	let setTimeAni;
+
+	const aniLoopFn = useMemo(()=> {
+		console.log(loopIdx);
+		if(loopIdx == null) {
+			setLoopIdx((loopIdx)=>0);
+		} else if(loopIdx >= 0) {		
+			loopIdx + 1 > 3 ? setLoopIdx((loopIdx)=>0) : setLoopIdx((loopIdx)=>loopIdx+1);
+		}
+		setTimeAni = setTimeout(aniLoopFn, 1000);
+	}, []);
 
 	useEffect(()=> {
+		// setTimeAni = setTimeout(aniLoopFn, 100);
+		setLoopIdx((loopIdx)=>0);
+		console.log(loopIdx);
+		console.log(loopIdx === 0);
+
 		introWrap.current.style.height = winH + 'px';
+		setObserveEl((observeEl)=>[...observeEl, introWrap.current]);
+
+		return ()=>{
+			clearTimeout(setTimeAni);
+		}
+
 	}, [introWrap]);
 
 	return(
 		<div className={'mainIntroWrap'} ref={introWrap}>
-			Intro
+			<span className={loopIdx === 0 ? 'unit active' : 'unit'}>Creative</span>
+			<span className={loopIdx === 1 ? 'unit active' : 'unit'}>Interactive</span>
+			<span className={loopIdx === 2 ? 'unit active' : 'unit'}>User Experience</span>
+			<span className={loopIdx === 3 ? 'unit active' : 'unit'}>We're J-one Lab</span>
 		</div>
 	);
+}
+
+IntroSection.propTypes = {
+	setObserveEl : PropTypes.func.isRequired
 }
 
 export default IntroSection;
