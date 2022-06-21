dab5e1d4d07cef2ba412ca6ff609cd4d9f6c77af
hyunh84 <hyunh84@hanmail.net>
2022-06-21 17:16:27 +0900

    2022/06/21 작업

--- src/Page/Home/IntroSection/index.js
+++ src/Page/Home/IntroSection/index.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const IntroSection = ({setObserveEl})=>{
-	const [loopIdx, setLoopIdx] = useState(null);
+	const [loopIdx, setLoopIdx] = useState(0);
 	const introWrap = useRef(null);
 	let winH = window.innerHeight;
 
@@ -10,8 +10,9 @@ const IntroSection = ({setObserveEl})=>{
 	useEffect(()=> {
 		introWrap.current.style.height = winH + 'px';
 		setObserveEl((observeEl)=>{
-			if(observeEl.indexOf(introWrap.current) > 0) {
-				observeEl.splice(observeEl.indexOf(introWrap.current), 0);
+			var hasEl = observeEl.indexOf(introWrap.current);
+			if(hasEl > 0) {
+				observeEl.splice(hasEl, 0);
 			}
 			return [...observeEl, introWrap.current];
 		});
@@ -19,10 +20,10 @@ const IntroSection = ({setObserveEl})=>{
 
 	return(
 		<div className={'mainIntroWrap'} ref={introWrap}>
-			<span className={loopIdx === 0 ? 'unit active' : 'unit'}>Creative</span>
-			<span className={loopIdx === 1 ? 'unit active' : 'unit'}>Interactive</span>
-			<span className={loopIdx === 2 ? 'unit active' : 'unit'}>User Experience</span>
-			<span className={loopIdx === 3 ? 'unit active' : 'unit'}>We're J-one Lab</span>
+			<span className={loopIdx === 1 ? 'unit active' : 'unit'}>Creative</span>
+			<span className={loopIdx === 2 ? 'unit active' : 'unit'}>Interactive</span>
+			<span className={loopIdx === 3 ? 'unit active' : 'unit'}>User Experience</span>
+			<span className={loopIdx === 34 ? 'unit active' : 'unit'}>We're J-one Lab</span>
 		</div>
 	);
 }
